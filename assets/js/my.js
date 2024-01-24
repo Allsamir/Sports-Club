9e13e9e55ea76d1ef4b86c02b3f31601f85f16ef
Al Samir <shahinur1591@gmail.com>
2024-01-24 16:59:43 +0600

    Half of the project done + responsive

---
+++ assets/js/my.js
@@ -0,0 +1,13 @@
+    tailwind.config = {
+      theme: {
+        extend: {
+          colors: {
+            red:'#FF4240',
+            borderColor: 'rgba(19, 19, 24, 0.20)',
+            overlay: 'rgba(19, 19, 24, 0.50)',
+            lightGreen: '#49D293',
+            productBorder: 'rgba(19, 19, 24, 0.10)'
+          }
+        }
+      }
+    }
